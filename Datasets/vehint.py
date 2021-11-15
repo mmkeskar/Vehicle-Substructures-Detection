2fd956c9a87bba94eb6b0c2da27e56ddb2f3af51
mmkeskar <54159799+mmkeskar@users.noreply.github.com>
2021-11-14 17:47:02 -0800

    Update vehint.py

--- Datasets/vehint.py
+++ Datasets/vehint.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov  4 14:54:11 2021
-
 @author: akshay
 """
 
@@ -17,8 +16,6 @@ import torch
 import json
 import cv2
 import os
-
-from helper_functions import gaussian_heatmap, get_radius
 """from utils.image import flip, color_aug
 from utils.image import get_affine_transform, affine_transform
 from utils.image import gaussian_radius, draw_umich_gaussian, draw_msra_gaussian
@@ -37,10 +34,10 @@ class VehIntDataset(data.Dataset):
         self.sample_size = len(self.images)
         
         self.max_objects = 49 # found using python script
-        self.num_kpts = 66
-        self.num_cats = 1
+        
         self.keypoint_ids = [0, 1, 2, 3, 22, 23, 25, 26, 31, 32, 34, 35, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65]
-        self.max_kpts = len(self.keypoint_ids)
+        self.num_kpts = len(self.keypoint_ids)
+        
         
         
     def _coco_box_to_bbox(self, box):
@@ -62,56 +59,43 @@ class VehIntDataset(data.Dataset):
         print(f"Image path is: {image_path}")
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
         
-        image_size = (image.shape[1], image.shape[2])
+        image_size = [image.shape[1], image.shape[2]]
         
         ann_id = self.coco.getAnnIds([image_id])
         annotations = self.coco.loadAnns(ann_id)
         
-        num_objs = min(self.max_objects, len(annotations))
-        
-        # a heatmap for the object centers
-        obj_hm = np.zeros((self.num_cats, image_size[0], image_size[1]), dtype=np.float32)
-        # a heatmap for all the keypoints
-        kpt_hm = np.zeros((self.max_kpts, image_size[0], image_size[1]), dtype=np.float32)
-        # the offsets of the keypoints from the object center points
-        kpt_regs = np.zeros((self.max_objects, self.max_kpts, 2), dtype=np.float)
+        # phi function that gives the heatmap for every keypoint
+        kpt_hm = np.zeros((self.num_kpts, image_size[0], image_size[1]), dtype=np.float32)
+        # J function that gives the offset from to the keypoints from the center of the object
         
         
-        ct_offset = np.zeros((self.max_objects, 2))
+        labels = []
         
-        for i in range(num_objs):
-            ann = annotations[i]
-            bbox = ann['bbox']
-            bbox = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]
-            
-            center = np.array([(bbox[0] + bbox[2])/2, (bbox[1] + bbox[3])/2])
-            ct_int = np.array([int(center[0]), int(center[1])])
-            
-            ct_offset[i] = center - ct_int
-            
-            radius = get_radius(bbox[2] - bbox[0], bbox[3] - bbox[1])
-            sigma = radius/3
-            
-            obj_hm[0] = gaussian_heatmap(obj_hm[0], ct_int, sigma)
-            
+        """bboxes = []
+        keypoints = []"""
+        for ann in annotations:
             if len(ann['keypoints']) != 0:
-                kpts = np.array(ann['keypoints']).reshape(self.num_kpts, 3)
-                
-                for j in range(self.max_kpts):
-                    kpt = kpts[self.keypoint_ids[j]]
-                    
-                    rad = get_radius(bbox[2] - bbox[0], bbox[3] - bbox[1])
-                    
-                    kpt_hm[j] = gaussian_heatmap(kpt_hm[j], (kpt[0], kpt[1]), rad)
-                    
-                    kpt_regs[i][j][0], kpt_regs[i][j][1] = kpt[0] - center[0], kpt[1] - center[1]
+                label = {}
+                label['bbox'] = np.array((ann['bbox']))
+                label['keypoints'] = []
+                for key_id in self.keypoint_ids:
+                    label['keypoints'].append([ann['keypoints'][3*key_id], ann['keypoints'][3*key_id + 1], ann['keypoints'][3*key_id + 2]])
+                label['keypoints'] = np.array(label['keypoints'])
+                labels.append(label)  
+        """bboxes = np.array(bboxes)   
+        keypoints = np.array(keypoints)
+        
+        if bboxes.shape[0] < self.max_objects:
+            padding = -np.ones((self.max_objects-bboxes.shape[0], bboxes.shape[1]))
+            bboxes = np.vstack((bboxes, padding))
             
-            
-           
-        sample = {'image': image, 'image_path': image_path, 'hm': obj_hm, \
-                  'center offset': ct_offset, 'keypoint regression': kpt_regs, \
-                  'keypoint heatmaps': kpt_hm} 
-            
+        if keypoints.shape[0] < self.max_objects*24:
+            padding = -np.ones((self.max_objects*24-keypoints.shape[0], keypoints.shape[1]))
+            keypoints = np.vstack((keypoints, padding))"""
+        
+        sample = {'image': image, 'image_path': image_path, 'labels': labels}
+        #print(f"bounding boxes matrix size: {bboxes.shape}")
+        #print(f"keypoints matrix size: {keypoints.shape}")
         return sample
     
     def __len__(self):
