2c76b560cf6d426d0aa6f2b2d3074f37b5cec7b0
mmkeskar <54159799+mmkeskar@users.noreply.github.com>
2021-11-16 14:39:55 -0800

    update helper_functions.py
    speeding up the gaussian heatmap generator and using radius from CornerNet

--- Datasets/helper_functions.py
+++ Datasets/helper_functions.py
@@ -9,7 +9,7 @@ Created on Sat Nov 13 16:22:42 2021
 import numpy as np
 import math
 
-def gaussian_heatmap(hm, center, sd):
+def gaussian_heatmap(heatmap, center, sigma):
     """
     hm : np array
         the heatmap to be updated
@@ -18,11 +18,26 @@ def gaussian_heatmap(hm, center, sd):
     sd : float
         standard deviation
     """
-    for index, x in np.ndenumerate(hm):
-        power = ((index[0] - center[0])**2 + (index[1] - center[1])**2)/(2*(sd**2))
-        hm[index] = max(x, np.exp(-power))
-        
-    return hm
+    h, w = heatmap.shape[0], heatmap.shape[1]
+    radius = sigma*3*math.sqrt(2)
+    ul = (max(0, int(center[0] - radius)), max(0, int(center[1] - radius)))
+    br = (min(h-1, int(center[0] + radius)), min(w-1, int(center[1] + radius)))
+    
+    num_rows = br[0]+1-ul[0]
+    num_cols = br[1]+1-ul[1]
+    
+    i = np.array(list(range(num_rows))*num_cols).reshape(num_cols, num_rows).transpose()
+    j = np.array(list(range(num_cols))*num_rows).reshape(num_rows, num_cols)
+    
+    powers = ((i - center[0])**2 + (j - center[1])**2)/(2*(sigma**2))
+    
+    dist = np.sqrt((i - center[0])**2 + (j - center[1])**2)
+    
+    mask = dist <= radius
+    temp = mask*np.exp(-powers)
+    
+    heatmap[ul[0]:br[0]+1, ul[1]:br[1]+1] = np.maximum(heatmap[ul[0]:br[0]+1, ul[1]:br[1]+1], temp)
+    
 
 def get_radius(h, w, min_IOU=0.3):
     a = 1
