fb7d760b7b8ed01241e00b2519f5aa7dad93a6ae
mmkeskar <54159799+mmkeskar@users.noreply.github.com>
2021-11-18 13:46:52 -0800

    update vehint.py to integrate with CenterNet code

--- Datasets/vehint.py
+++ Datasets/vehint.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov  4 14:54:11 2021
+
 @author: akshay
 """
 
@@ -19,7 +20,6 @@ import os
 
 from utils.image import gaussian_radius, draw_umich_gaussian, draw_msra_gaussian
 from utils.image import draw_dense_reg
-
 """from utils.image import flip, color_aug
 from utils.image import get_affine_transform, affine_transform
 from utils.image import gaussian_radius, draw_umich_gaussian, draw_msra_gaussian
@@ -27,36 +27,6 @@ from utils.image import draw_dense_reg"""
 import math
 
 class VehIntDataset(data.Dataset):
-    
-    def __init__(self, data_dir, opt):
-        super(VehIntDataset, self).__init__()
-        self.data_dir = data_dir
-        self.opt = opt
-        
-        self.image_path_const = "/media/akshay/SSD1/data-apollocar3d/train/images"
-        self.coco = coco.COCO(self.data_dir)
-        self.images = self.coco.getImgIds()
-        
-        self.sample_size = len(self.images)
-        
-        self.max_objects = 49 # found using python script
-        
-        self.keypoint_ids = [0, 1, 2, 3, 22, 23, 25, 26, 31, 32, 34, 35, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65]
-        self.num_kpts = len(self.keypoint_ids)
-        
-        
-        
-    def _coco_box_to_bbox(self, box):
-        bbox = np.array([box[0], box[1], box[0] + box[2], box[1] + box[3]],
-                    dtype=np.float32)
-        return bbox
-
-    def _get_border(self, border, size):
-        i = 1
-        while size - border // i <= border // i:
-            i *= 2
-            return border // i
-
 
     def __getitem__(self, index):
         image_id = self.images[index]
@@ -64,16 +34,14 @@ class VehIntDataset(data.Dataset):
         image_path = os.path.join(self.image_path_const, file_path_ext)
         print(f"Image path is: {image_path}")
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    
+        
         output_res = self.opt.output_res
         
         ann_id = self.coco.getAnnIds([image_id])
         annotations = self.coco.loadAnns(ann_id)
         
-        # phi function that gives the heatmap for every keypoint
-        kpt_hm = np.zeros((self.num_kpts, image_size[0], image_size[1]), dtype=np.float32)
-        # J function that gives the offset from to the keypoints from the center of the object
-
+        num_objs = min(self.max_objects, len(annotations))
+        
         # a heatmap for the object centers
         hm = np.zeros((self.num_cats, output_res, output_res), dtype=np.float32)
         # a heatmap for all the keypoints
@@ -149,7 +117,3 @@ class VehIntDataset(data.Dataset):
                   'kps_mask': kps_mask, 'gt_det': gt_det, 'ind': ind, 'hp_ind': hp_ind} 
             
         return sample
-        
-        
-    def __len__(self):
-       return self.sample_size
