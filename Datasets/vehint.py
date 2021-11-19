875bb1db07a6e0e1651e44aff119d36ace7c39e3
mmkeskar <54159799+mmkeskar@users.noreply.github.com>
2021-11-19 14:51:45 -0800

    final vehint

--- Datasets/vehint.py
+++ Datasets/vehint.py
@@ -29,10 +29,11 @@ import math
 class VehIntDataset(data.Dataset):
 
     def __getitem__(self, index):
+        #print("__get_item__ called")
         image_id = self.images[index]
         file_path_ext = self.coco.loadImgs(image_id)[0]['file_name']
         image_path = os.path.join(self.image_path_const, file_path_ext)
-        print(f"Image path is: {image_path}")
+        #print(f"Image path is: {image_path}")
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
         
         output_res = self.opt.output_res
@@ -48,8 +49,8 @@ class VehIntDataset(data.Dataset):
         hm_hp = np.zeros((self.max_kpts, output_res, output_res), dtype=np.float32)
         # the offsets of the keypoints from the object center points
         kps = np.zeros((self.max_objects, self.max_kpts*2), dtype=np.float)
-        reg_mask = np.zeros((self.max_objects), dtype=np.unit8)
-        kps_mask = np.zeros((self.max_objects, self.max_kpts*2), dtype=np.unit8)
+        reg_mask = np.zeros((self.max_objects), dtype=np.uint8)
+        kps_mask = np.zeros((self.max_objects, self.max_kpts*2), dtype=np.uint8)
         wh = np.zeros((self.max_objects, 2), dtype=np.float32)
         hp_mask = np.zeros((self.max_objects*self.max_kpts), dtype=np.int64)
         reg = np.zeros((self.max_objects, 2))
@@ -64,7 +65,7 @@ class VehIntDataset(data.Dataset):
         
         draw_gaussian = draw_msra_gaussian if self.opt.mse_loss else draw_umich_gaussian
         
-        print("creating targets")
+        #print("creating targets")
         for i in range(num_objs):
             ann = annotations[i]
             bbox = ann['bbox']
@@ -80,40 +81,65 @@ class VehIntDataset(data.Dataset):
             
             wh[i] = w, h
             
-            radius = gaussian_radius((h, w))
+            radius = gaussian_radius((math.ceil(h), math.ceil(w)))
             sigma = radius/3
             
             
             if len(ann['keypoints']) != 0:
                 kpts = np.array(ann['keypoints']).reshape(self.num_kpts, 3)
+                kpts = kpts[self.keypoint_ids, :]
                 reg_mask[i] = 1
                 
                 for j in range(self.max_kpts):
-                    kpt = kpts[self.keypoint_ids[j]]
+                    kpt = kpts[j]
                     
                     hp_offset[i*self.max_kpts+j] = kpt[:2] - kpt[:2].astype(np.int32)
                     
-                    draw_gaussian(hm_hp[j], (kpt[0], kpt[1]), radius)
+                    draw_gaussian(hm_hp[j], (int(kpt[0]), int(kpt[1])), radius)
                     
                     kps[i][j*2:j*2+2] = kpt[0] - center[0], kpt[1] - center[1]
                     
-                    hp_mask[i*self.max_keypoints+j] = 1
+                    hp_mask[i*self.max_kpts+j] = 1
                     
                     kps_mask[i][j*2:j*2+2] = 1
                     
                     hp_ind[i*self.max_kpts + j] = int(kpt[1])*output_res + int(kpt[0])
                     
                     if self.opt.dense_hp:
-                        draw_dense_reg(dense_kps, hm[0], ct_int, kps[i][j*2:j*2+2], radius, True)
-                        draw_gaussian(dense_kps_mask, ct_int, radius)
+                        draw_dense_reg(dense_kps[j], hm[0], ct_int, kps[i][j*2:j*2+2], radius, True)
+                        draw_gaussian(dense_kps_mask[j], ct_int, radius)
                 
                 gt_det.append([center[0] - w/2, center[1] - h/2,
-                           center[0] + w/2, center[1] + h/2, 1] + kpts[:, :2].reshape[self.max_kpts*2].tolist() + [0])
+                           center[0] + w/2, center[1] + h/2, 1] + kpts[:, :2].reshape(self.max_kpts*2).tolist() + [0])
            
             draw_gaussian(hm[0], ct_int, sigma)
-            
-        sample = {'image': image, 'image_path': image_path, 'hm': hm, 'wh': wh, 'hp_mask': hp_mask, \
-                  'reg': reg, 'kps': kps, 'hm_hp': hm_hp, 'reg_mask': reg_mask, \
-                  'kps_mask': kps_mask, 'gt_det': gt_det, 'ind': ind, 'hp_ind': hp_ind} 
-            
-        return sample
+        
+        ret = {'input': image, 'hm': hm, 'reg_mask': reg_mask, 'ind': ind, 'wh': wh,
+               'hps': kps, 'hps_mask': kps_mask}
+        if self.opt.dense_hp:
+          dense_kps = dense_kps.reshape(self.max_kpts * 2, output_res, output_res)
+          dense_kps_mask = dense_kps_mask.reshape(
+            self.max_kpts, 1, output_res, output_res)
+          dense_kps_mask = np.concatenate([dense_kps_mask, dense_kps_mask], axis=1)
+          dense_kps_mask = dense_kps_mask.reshape(
+            self.max_kpts * 2, output_res, output_res)
+          ret.update({'dense_hps': dense_kps, 'dense_hps_mask': dense_kps_mask})
+          del ret['hps'], ret['hps_mask']
+        if self.opt.reg_offset:
+          ret.update({'reg': reg})
+        if self.opt.hm_hp:
+          ret.update({'hm_hp': hm_hp})
+        if self.opt.reg_hp_offset:
+          ret.update({'hp_offset': hp_offset, 'hp_ind': hp_ind, 'hp_mask': hp_mask})
+        if self.opt.debug > 0 or not self.split == 'train':
+          gt_det = np.array(gt_det, dtype=np.float32) if len(gt_det) > 0 else \
+                   np.zeros((len(gt_det), len(gt_det[0])), dtype=np.float32)
+          meta = {'gt_det': gt_det, 'img_id': image_id}
+          ret['meta'] = meta
+          
+        """
+        for key, value in ret.items():
+            if value is None:
+                print(f"this is returning a None: {key} for image id: {image_id}")  // for debugging
+        """
+        return ret
