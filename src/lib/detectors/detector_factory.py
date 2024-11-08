5de37d03307297f4d4b0641d30331dcabb5c38be
mmkeskar <54159799+mmkeskar@users.noreply.github.com>
2024-11-07 20:05:20 -0800

    Update detector_factory.py

--- src/lib/detectors/detector_factory.py
+++ src/lib/detectors/detector_factory.py
@@ -8,6 +8,8 @@ from .ctdet import CtdetDetector
 from .multi_pose import MultiPoseDetector
 from .base_detector_vehint import VehintDetector
 from .vehint_kptreg_detector import VehintKptRegDetector
+from .vehint_taillight_detector import VehintTaillightDetector
+from .cascaded_model_detector import CascadedModelDetector
 
 detector_factory = {
   'exdet': ExdetDetector, 
@@ -15,5 +17,8 @@ detector_factory = {
   'ctdet': CtdetDetector,
   'multi_pose': MultiPoseDetector,
   'vehint': VehintDetector,
-  'vehint_kptreg': VehintKptRegDetector
+  'vehint_kptreg': VehintKptRegDetector,
+  'vehint_taillight': VehintTaillightDetector,
+  'offline_model1': VehintDetector,
+  'cascaded': CascadedModelDetector
 }
