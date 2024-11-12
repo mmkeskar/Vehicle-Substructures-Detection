d6761e246e0f7796fca621c626aa88c114bf08fc
mmkeskar <54159799+mmkeskar@users.noreply.github.com>
2024-11-12 12:08:28 -0800

    Update dataset_factory.py

--- src/lib/datasets/dataset_factory.py
+++ src/lib/datasets/dataset_factory.py
@@ -3,36 +3,21 @@ from __future__ import division
 from __future__ import print_function
 
 from .sample.vehint import VehIntDataset
-from .sample.ddd import DddDataset
-from .sample.exdet import EXDetDataset
-from .sample.ctdet import CTDetDataset
-from .sample.multi_pose import MultiPoseDataset
-from .sample.vehint2 import VehIntDataset2
 from .sample.vehint_kptreg import VehIntKptRegDataset
+from .sample.offline_model1 import VehInt6KptRegDataset
 
 from .dataset.apollo3d import Apollo3d
-from .dataset.coco import COCO
-from .dataset.pascal import PascalVOC
-from .dataset.kitti import KITTI
-from .dataset.coco_hp import COCOHP
 
 
 dataset_factory = {
-  'apollo': Apollo3d,
-  'coco': COCO,
-  'pascal': PascalVOC,
-  'kitti': KITTI,
-  'coco_hp': COCOHP
+  'apollo': Apollo3d
 }
 
 _sample_factory = {
   'vehint': VehIntDataset,
-  'exdet': EXDetDataset,
-  'ctdet': CTDetDataset,
-  'ddd': DddDataset,
-  'multi_pose': MultiPoseDataset,
-  'vehint2': VehIntDataset2,
-  'vehint_kptreg': VehIntKptRegDataset
+  'vehint_kptreg': VehIntKptRegDataset,
+  'offline_model1': VehInt6KptRegDataset,
+  'cascaded': VehInt6KptRegDataset
 }
 
 
